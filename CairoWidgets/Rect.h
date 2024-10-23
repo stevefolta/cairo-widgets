d6044488f72d163e7876b47d387908bb39fda35d
Steve Folta <steve@folta.net>
2024-10-23 16:32:40 -0700

    Rect: added handy helper functions.

--- CairoWidgets/Rect.h
+++ CairoWidgets/Rect.h
@@ -10,5 +10,17 @@ struct Rect {
 			pt_x >= x && pt_x < x + width &&
 			pt_y >= y && pt_y < y + height;
 		}
+
+	void shrink_left_by(double amount) {
+		x += amount;
+		width -= amount;
+		}
+	void shrink_top_by(double amount) {
+		y += amount;
+		height -= amount;
+		}
+
+	double left() { return x + width; }
+	double bottom() { return y + height; }
 	};
 
